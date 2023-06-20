a702f49bf3bccf49c06ed394c7c0f3d9d3bf3126
dvorkinguy <dvorkin.guy@gmail.com>
2023-06-20 13:40:13 +0300

    first commit

---
+++ k8s-adv/lesson-6-dockerimage/index.php
@@ -0,0 +1,9 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello From K8s</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>Guy Dvorkin";
+echo "Made by <font color=blue>v2.0";
+?>
+
